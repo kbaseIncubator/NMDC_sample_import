7d57df3ce57ec3871bcb126fc9e17778facfed2d
realmarcin <4625870+realmarcin@users.noreply.github.com>
2020-08-27 13:02:48 -0700

    now with mapping to KBase metadata fields
    adding example of mapped tsv

--- scripts/parseNMDC.py
+++ scripts/parseNMDC.py
@@ -14,45 +14,57 @@ def map_to_kbase_tsv(nmdc_db, nmdc_to_kbase__map):
         outstr = ""
         for field in sample:
             print("field " + field)
-
-            mapcol = nmdc_to_kbase__map.loc[:, nmdc_to_kbase__map.columns.str.contains(field)]
+            searchstr = "^"+field+"$"
+            print(searchstr)
+            mapcol = nmdc_to_kbase__map.filter(regex=searchstr)#nmdc_to_kbase__map.columns.str.findall("^"+field+"$")
             print(mapcol)
-            print(type(mapcol))
-            print(mapcol.iloc[0][1])
-            #exit(0)
+            #print(mapcol.iloc[1])
+            #print(mapcol.iloc[1].index.tolist())
+            #print(str(mapcol.iloc[1].index))
+
+            #print(type(mapcol.iloc[0]))
+            #mapcol = nmdc_to_kbase__map[index]#nmdc_to_kbase__map.loc[:, index]
+            #print(mapcol)
+
+            if (not mapcol.empty):
+                mapcolfield = mapcol.iloc[0][0]
+                print("mapcolfield "+mapcolfield)
+                #print(mapcol.iloc[1])
+                #print(sample[field])
+
+                if not type(sample[field]) == str:
+                    print(len(sample[field]))
+                    # if len(sample[field]) == 2:
 
 
-            print(sample[field])
-            print(type(sample[field]))
-            if not type(sample[field]) == str:
-                print(len(sample[field]))
-                # if len(sample[field]) == 2:
-                for field2 in sample[field]:
-                    print("field2 " + field2)
-                    print(sample[field][field2])
-                    #if len(sample[field]) == 1:
-                    mapcol = nmdc_to_kbase__map.loc[:, nmdc_to_kbase__map.columns.str.contains(field2)]
-                    header = header + mapcol.iloc[0][1] + "\t"
-                    #else:
-                    #header = header + field + "_" + field2 + "\t"
+                    for field2 in sample[field]:
+                        print("field2 " + field2)
+                        print(sample[field][field2])
 
-                    outstr = outstr + str(sample[field][field2]) + "\t"
-                    # else if field = "lat_lon":
-                    #    for field2 in sample[field]:
-                    #        print(field2)
-                    #        print(sample[field][field2])
-                    #        header = header + field2+"\t"
-                    #        outstr = outstr + sample[field][field2]+"\t"
-            else:
-                header = header + mapcol.iloc[0][1] + "\t"
-                outstr = outstr + sample[field] + "\t"
+                        header = header + mapcolfield  + "\t"
+                        #else:
+                        #header = header + field + "_" + field2 + "\t"
+
+                        outstr = outstr + str(sample[field][field2]) + "\t"
+                        # else if field = "lat_lon":
+                        #    for field2 in sample[field]:
+                        #        print(field2)
+                        #        print(sample[field][field2])
+                        #        header = header + field2+"\t"
+                        #        outstr = outstr + sample[field][field2]+"\t"
+                else:
+                    header = header + mapcolfield+ "\t"
+                    outstr = outstr + sample[field] + "\t"
 
 
+        print(str(header))
+        print(outstr)
 
-        outf.write(header + "\n")
+        outf.write(str(header) + "\n")
         outf.write(outstr + "\n")
 
         outf.close()
+        #exit(0)
 
 def flatten_to_tsv(nmdc_db):
     print("len(nmdc_db[\"biosample_set\"])) " + str(len(nmdc_db["biosample_set"])))
